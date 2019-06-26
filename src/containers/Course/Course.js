811d079f4f973032a8a02ba3426f76d3f8a3a722
Konstantin <konstantin.v.modin@gmail.com>
2019-06-26 15:39:21 +0200

    updates

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Course.css';
 
-const Course = (props) => (
+const Course = (props) => 
             <div className="FullCourse">
                 <h1>{props.location.search.split`%20`.join` `.slice(2)}</h1>
-                <h2>You selected the Course with ID: {props.match.url.split`/`[2]}</h2>
-            </div>
-        );
+                <h2>You selected the Course with ID: {props.match.params.id}</h2>
+            </div>;        
 
 export default Course;
